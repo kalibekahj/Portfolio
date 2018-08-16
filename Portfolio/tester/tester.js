7e46643b44382934ba60c5d4c92b5f56deb3f4d7
Kalijah <kalijahjonesr2h@rv-clt-kajones.redventures.net>
2018-08-15 23:53:18 -0400

    adding 8 portfolio files

---
+++ Portfolio/tester/tester.js
@@ -0,0 +1,7 @@
+var hamburger = document.querySelector('.hamburger');
+var hamburgerNav = document.querySelector('.hamburger-nav');
+
+hamburger.addEventListener("click", function(){
+    hamburgerNav.classList.toggle("displayNav");
+});
+
